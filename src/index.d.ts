f2faedeecbd1b0d37c2d1a3801a3b78b0ced2ccf
kristinnornk <58362287+kristinnornk@users.noreply.github.com>
2023-09-05 10:40:51 +0000

    ...

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+export * from "./src/types/datatypes";
+export * from "./src/types/converters";
+export * from "./src/constants/conditions";
+export * from "./src/constants/outcomeMeasures";
+export * from "./src/constants/programs";
