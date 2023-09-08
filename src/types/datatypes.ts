d41e77e2b9a1da1e9fa61ca013c0f7548f99defc
kristinnornk <58362287+kristinnornk@users.noreply.github.com>
2023-09-08 11:03:58 +0000

    ...

--- src/types/datatypes.ts
+++ src/types/datatypes.ts
@@ -149,7 +149,7 @@ type TProgramBase = {
   conditionId: TConditionId;
   outcomeMeasureIds: TOutcomeMeasureId[];
   // TODO: ræða hvort days eigi að vera hér inni eða ekki.
-  days: { [key: `d${number}`]: TProgramDay };
+  days: { [key: `${number}`]: TProgramDay };
   // programPath: TProgramPath // * "programs/plantar-heel-pain" or "physios/physio1/programs/plantar-heel-pain"
 };
 
