b5b45bd27fa29cfa63f1c0f8140edeb5b41a9848
Kjartan Örn Bogason <kjartanorn99@gmail.com>
2023-09-11 13:36:53 +0000

    fixes

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "./types/datatypes";
 export * from "./constants/conditions";
 export * from "./constants/outcomeMeasures";
 export * from "./constants/programs";
-export * from "./utilities/functions";
+export * from "./utilities/firestore";
 export * from "./utilities/converters";
