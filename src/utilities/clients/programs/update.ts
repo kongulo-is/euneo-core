7e1657995050d33742c8263378e8dfb371efeec3
kristinnornk <58362287+kristinnornk@users.noreply.github.com>
2023-10-03 13:53:04 +0000

    Create update.ts

---
+++ src/utilities/clients/programs/update.ts
@@ -0,0 +1,38 @@
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../../firebase/db";
+
+export async function updateProgramDay(
+  clientId: string,
+  clientProgramId: string,
+  dayId: string,
+  //TODO: fix type..
+  exercises: any,
+  adherence: number
+) {
+  try {
+    const day = doc(
+      db,
+      "clients",
+      clientId,
+      "programs",
+      clientProgramId,
+      "days",
+      dayId.toString()
+    );
+
+    updateDoc(day, {
+      adherence: adherence,
+      // TODO: create a converter
+      exercises: exercises.map((e: any) => e.iteration),
+    });
+  } catch (error) {
+    console.error("Error updating program day: ", error, {
+      clientId,
+      clientProgramId,
+      dayId,
+      exercises,
+      adherence,
+    });
+    throw error;
+  }
+}
