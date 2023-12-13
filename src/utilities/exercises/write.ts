9b96dbeb64f4f3566483de97a5ec6831546edac9
kristinnornk <58362287+kristinnornk@users.noreply.github.com>
2023-12-13 14:31:16 +0000

    Update write.ts

--- src/utilities/exercises/write.ts
+++ src/utilities/exercises/write.ts
@@ -1,4 +1,11 @@
-import { collection, CollectionReference, getDocs } from "firebase/firestore";
+import {
+  collection,
+  CollectionReference,
+  getDocs,
+  doc,
+  DocumentReference,
+  getDoc,
+} from "firebase/firestore";
 import { db } from "../../firebase/db";
 import { TExercise, TExerciseWrite } from "../../types/baseTypes";
 import { exerciseConverter } from "../converters";
@@ -22,6 +29,25 @@ export async function getAllExercises(): Promise<Record<string, TExercise>> {
     return exercises;
   } catch (error) {
     console.error("Error fetching exercises:", error);
+    throw error;
+  }
+}
+
+export async function getExerciseById(id: string): Promise<TExercise> {
+  try {
+    const exerciseRef = doc(
+      db,
+      "exercises",
+      id
+    ) as DocumentReference<TExerciseWrite>;
+    const exerciseSnap = await getDoc(
+      exerciseRef.withConverter(exerciseConverter)
+    );
+    const exercise = exerciseSnap.data();
+    if (!exercise) throw new Error(`Exercise with id ${id} not found`);
+    return exercise;
+  } catch (error) {
+    console.error("Error fetching exercise:", error);
     throw error;
   }
 }
