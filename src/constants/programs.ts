24e434c42634cdaaecf193d45beaba2af6d291c0
fridriks98 <fridriks98@gmail.com>
2023-12-19 14:38:54 +0000

    Update programs.ts

--- src/constants/programs.ts
+++ src/constants/programs.ts
@@ -5,10 +5,11 @@ type Programs = Record<TEuneoProgramId, string>;
 export const programs: Programs = {
   "plantar-heel-pain": "Plantar Heel Pain",
   "severs-disease": "Sever's disease",
-  "plantar-fasciitis": "Plantar Fasciitis",
-  "achilles-tendinopathy": "Achilles Tendinopathy",
-  "osgood-schlatter-disease": "Osgood-Schlatter Disease",
-  "lateral-ankle-sprain": "Lateral Ankle Sprain",
+  "plantar-fasciitis": "Plantar fasciitis",
+  "achilles-tendinopathy": "Achilles tendinopathy",
+  "osgood-schlatter-disease": "Osgood-Schlatter disease",
+  "lateral-ankle-sprain": "Lateral ankle sprain",
+  "knee-replacement": "Knee replacement",
 };
 
 export const programOptions = Object.entries(programs).map(
