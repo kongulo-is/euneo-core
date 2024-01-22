956e97033ffa131fc22abec20906dc50c43a26ce
kristinnornk <58362287+kristinnornk@users.noreply.github.com>
2024-01-22 10:12:12 +0000

    Remove invitations

--- src/utilities/invitations/delete.ts
+++ src/utilities/invitations/delete.ts
@@ -1,4 +1,5 @@
 import {
+  DocumentReference,
   collection,
   deleteDoc,
   doc,
@@ -17,4 +18,36 @@ export async function removeInvitation(code: string) {
 
   const docRef = doc(db, "invitations", docId);
   await deleteDoc(docRef);
+}
+
+// TODO: TEMPORARY - Remove all invitations for a list of clinicians
+export async function clearInvitations(clinicians: string[]) {
+  const invitations: DocumentReference[] = [];
+  const invitationsToDelete: DocumentReference[] = [];
+  const invitationRef = collection(db, "invitations");
+  const data = await getDocs(query(invitationRef));
+
+  data.forEach((doc) => {
+    const path = doc.data().clinicianClientRef.path;
+    if (
+      clinicians.some((c) => {
+        return path.includes(c);
+      })
+    ) {
+      console.log(doc.data().clinicianClientRef.path);
+      invitations.push(doc.ref);
+    } else {
+      invitationsToDelete.push(doc.ref);
+    }
+  });
+
+  console.log("To delete", invitationsToDelete);
+  console.log("To keep", invitations);
+
+  // Delete
+  invitationsToDelete.forEach(async (i) => {
+    await deleteDoc(i);
+  });
+
+  return invitations;
 }
