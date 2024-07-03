cb84eaab68836b3085835c9dd2b77127a131d9fa
fridriks98 <fridriks98@gmail.com>
2024-07-03 15:42:08 +0000

    Create delete.ts

---
+++ src/utilities/clients/programs/delete.ts
@@ -0,0 +1,28 @@
+import { DocumentReference, deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../../firebase/db";
+import { TClientProgramWrite } from "../../../types/clientTypes";
+
+export async function removeDaysFromClientProgram(
+  clientId: string,
+  clientProgramId: string,
+  firstDocIndex: number,
+  length: number
+) {
+  const programRef = doc(
+    db,
+    "clients",
+    clientId,
+    "programs",
+    clientProgramId
+  ) as DocumentReference<TClientProgramWrite>;
+  // Update days documents
+  const tempArr = Array.from({ length }, (_, i) => i + firstDocIndex);
+  console.log("tempArr", tempArr);
+
+  await Promise.all(
+    tempArr.map((dayIndex) => {
+      const dayCol = doc(programRef, "days", dayIndex.toString());
+      return deleteDoc(dayCol);
+    })
+  );
+}
