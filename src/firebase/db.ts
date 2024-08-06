ceba0da1474d28dda13aee73cd077bd99af0cdbb
Kjartan Örn Bogason <kjartanorn99@gmail.com>
2024-08-06 14:42:44 +0000

    yes

--- src/firebase/db.ts
+++ src/firebase/db.ts
@@ -10,21 +10,14 @@ export const db = initializeFirestore(app, {
 
 const env = process.env.NODE_ENV;
 
-if (env === "development") {
+console.log(
+  "process.env.EXPO_PUBLIC_USE_EMULATOR",
+  process.env.EXPO_PUBLIC_USE_EMULATOR
+);
+
+if (env === "development" && process.env.EXPO_PUBLIC_USE_EMULATOR === "true") {
   console.log("Connecting to Firestore emulator");
 
   // Connect Firebase Auth to the local emulator
-  connectFirestoreEmulator(db, "localhost", 8080);
+  connectFirestoreEmulator(db, "192.168.1.240", 8080);
 }
-
-// console.log(
-//   "process.env.NEXT_PUBLIC_EMULATOR",
-//   process.env.NEXT_PUBLIC_EMULATOR
-// );
-
-// if (process.env.NEXT_PUBLIC_EMULATOR) {
-//   // Connect Firebase Auth to the local emulator
-//   console.log("Connecting to Firestore emulator");
-
-//   connectFirestoreEmulator(db, "localhost", 8080);
-// }
